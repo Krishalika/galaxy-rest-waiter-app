4325f0bec811a22e2ba4f66896eab0bfdf112200
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-07-23 10:44:03 +0530

    initial commits

---
+++ Navigators/DrawerNavigation.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { ViewBase, StyleSheet, ScrollView, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+
+export default function DrawerNavigation() {
+  return <View></View>;
+}
