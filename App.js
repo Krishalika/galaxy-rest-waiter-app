addebf894c38f2fc66e755eb43bc85271aa1a94d
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-09-23 00:13:47 +0530

    EDIT - login page

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 
 //components
 import DrawerNavigation from "./Navigators/DrawerNavigation";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./Screens/Login";
+
+const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="Login"
+//           component={Login}
+//           // options={{ headerShown: false }}
+//         />
+//         <Stack.Screen name="DrawerNavigation" component={DrawerNavigation} />
+//         {/* <DrawerNavigation />; */}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+//   // return <DrawerNavigation />;
+// }
 
 export default function App() {
   return <DrawerNavigation />;
